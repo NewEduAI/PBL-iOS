0ff1e763e72c59adf3941f03db7767f991ffac02
Daniel Zhang-Li <zlnn17@tsinghua.org.cn>
2026-01-21 10:32:25 +0800

    Initial Commit

---
+++ PBL/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PBL
+//
+//  Created by Daniel Zhang-Li on 2026/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PBL/PBLApp.swift
@@ -0,0 +1,17 @@
+//
+//  PBLApp.swift
+//  PBL
+//
+//  Created by Daniel Zhang-Li on 2026/1/21.
+//
+
+import SwiftUI
+
+@main
+struct PBLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
